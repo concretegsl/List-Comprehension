a4c2b46854408ce09107572094f1f6baa77a7b76
Gabriel <gabriel.sloza@gmail.com>
2026-04-23 10:42:20 -0700

    Create list overlap comprehensions.py

---
+++ list overlap comprehensions.py
@@ -0,0 +1,38 @@
+import random 
+
+# Get lenght of lists from user
+lenA = int(input("Give me the length of list A: "))
+lenB = int(input("Give me the length of list B: "))
+
+# Fill list A and B with random integers and lenght according to user input
+a = [random.randint(1,10) for _ in range(lenA)]
+b = [random.randint(1,10) for _ in range(lenB)]
+
+# Declare set 'c'
+c = set()
+
+# Fill set with numbers from A and B, then print all 3 sorted
+c = set(a + b)
+print(f"Sorted list A: {sorted(a)}")
+print(f"Sorted list B: {sorted(b)}")
+print(f"Sorted set C: {sorted(c)}")
+
+# Print only even numbers
+cEven = {item for item in (a + b) if item % 2 == 0}
+print(f"Sorted set C (only even numbers): {sorted(cEven)}")
+
+# Print the sum off the lists A, B and set C
+sumA = sum(a)
+sumB = sum(b)
+sumC = sum(c)
+sumCEven = sum(cEven)
+print(f"The sum of list A is: {sumA}")
+print(f"The sum of list B is: {sumB}")
+print(f"The sum of set C is: {sumC}")
+print(f"The sum of the even numbers from set C is: {sumCEven}")
+
+
+
+
+
+
